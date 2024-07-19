82e771b982b1f6fc34bf97318f59224e01152487
maferadr <maferadr0604@gmail.com>
2024-07-18 22:32:19 -0400

    Loginn and auth credetentials done

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect } from "react";
 //Testing to display the Workout List on the screen depending on the user input at handling click
 import WorkoutsList from "./WorkoutsList";
+import SignUpForm from "./forms/signUpForm";
 
 
 export default function Login(){
@@ -27,12 +28,13 @@ export default function Login(){
     }, []);
 
     return(
-        <div className="bg-black h-screen">
+        <div className="bg-black h-100 lg:h-screen">
             <div className="gradient-black login-gradient"></div>
             <img className="login-img object-cover" src="https://images.pexels.com/photos/4753996/pexels-photo-4753996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
         
-            <div className="login-container flex justify-center pt-20">
-                <h1 className="text-white title-1">Join us</h1>
+            <div className="login-container pt-20">
+                <h1 className="text-white title-1 flex justify-center">Join us</h1>
+                <SignUpForm/>
                 <WorkoutsList workouts={workouts} setWorkouts={setWorkouts} title='Enjoy'/>
                 
             </div>
